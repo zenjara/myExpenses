67bd9a1cb66ae926ceeaafd5671a3d5d88d81443
ivanmatas <ivan.matas@locastic.com>
2018-02-12 22:08:33 +0100

    Initial project commit + model setup

---
+++ src/AppBundle/Entity/MonthlyLimit.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * MonthlyLimit
+ */
+class MonthlyLimit
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var float
+     */
+    private $amount;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param float $amount
+     *
+     * @return MonthlyLimit
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
+
