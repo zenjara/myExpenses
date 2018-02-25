d2717efccb1995eaf423a79f4dac5857e4f9b0ba
Kristijan Korac <korac.kristijan@gmail.com>
2018-02-25 18:18:59 +0100

    restricting first routes

--- myexpenses-react/src/components/App/App.js
+++ myexpenses-react/src/components/App/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Login from "../Login";
+import Main from "../Main";
 
 class App extends Component {
     render() {
-        return [
-            //<Route exact path='/' component={Login} key='dashboard' />,
-            <Route path='/login' component={Login} key='login' />
-        ];
+        return (
+            <Switch>
+                <Route exact path='/' render={props => (
+                    this.props.authenticated ? <Main {...props} /> : <Redirect to='/login' />
+                )} />,
+                <Route path='/login' render={props => (
+                    this.props.authenticated ? <Redirect to='/' /> : <Login {...props} />
+                )} />
+            </Switch>
+        );
     }
 }
 
-export default App;
+function mapStateToProps({ auth }) {
+    return { authenticated: auth.authenticated };
+}
+
+export default withRouter(connect(mapStateToProps)(App));
--- myexpenses-react/src/reducers/authReducer.js
+++ myexpenses-react/src/reducers/authReducer.js
@@ -5,7 +5,7 @@ import {
     RESET_LOGGING_USER
 } from "../actions/types";
 
-export default function(state = {}, action) {
+export default function(state = { authenticated: false }, action) {
     switch(action.type) {
         case AUTH_USER:
             return { ...state, authenticated: true };
