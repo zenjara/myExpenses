dde2cba8efc17e55407f68c0f6b52e1abd1c4b7e
Kristijan Korac <korac.kristijan@gmail.com>
2018-04-21 21:00:53 +0200

    adding dashboard window component

--- myexpenses-react/src/components/MainWindow/MainWindow.js
+++ myexpenses-react/src/components/MainWindow/MainWindow.js
@@ -2,19 +2,17 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 // import { connect } from 'react-redux';
 
+import Dashboard from '../Dashboard';
 import UserInfo from '../UserInfo';
 import Expenses from '../Expenses';
 
 class MainWindow extends Component {
-    constructor(props) {
-        super(props);
-
-    }
 
     render() {
         return (
             <div className="main-app__window">
                 <Switch>
+                    <Route exact path='/' component={Dashboard} />
                     <Route path='/me' component={UserInfo} />
                     <Route path='/expenses' component={Expenses} />
                 </Switch>
