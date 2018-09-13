09a03cd3c008d697cfceb097dae2246fb92eae93
ivanmatas <ivan.matas@arsfutura.co>
2018-09-13 20:39:48 +0200

    Add authentication and authorization with jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
 	has_many :expenses
 	has_one :daily_limit
 	has_one :monthly_limit
+
+	validates_presence_of :name, :email, :password_digest
+	validates :email, uniqueness: true
+
+	#encrypt password
+	has_secure_password
 end
