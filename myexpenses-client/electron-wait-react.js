d079ddaf0271a74c71c0547687866edd25445b91
Kristijan Korac <korac.kristijan@gmail.com>
2018-10-29 21:54:38 +0100

    Electron run implemented

---
+++ myexpenses-client/electron-wait-react.js
@@ -0,0 +1,27 @@
+const net = require('net');
+const port = process.env.PORT ? process.env.PORT - 100 : 8000;
+
+process.env.ELECTRON_START_URL = `http://localhost:${port}/index.electron.html`;
+
+const client = new net.Socket();
+
+let startedElectron = false;
+const tryConnection = () =>
+  client.connect(
+    { port: port },
+    () => {
+      client.end();
+      if (!startedElectron) {
+        console.log('starting electron');
+        startedElectron = true;
+        const exec = require('child_process').exec;
+        exec('npm run electron');
+      }
+    }
+  );
+
+tryConnection();
+
+client.on('error', error => {
+  setTimeout(tryConnection, 1000);
+});
