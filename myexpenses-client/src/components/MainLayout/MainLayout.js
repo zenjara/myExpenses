fb55d788a24071118cde1165cb5ca59bde7701a1
Kristijan Korac <korac.kristijan@gmail.com>
2018-11-05 22:25:59 +0100

    Sidebar in progres...

---
+++ myexpenses-client/src/components/MainLayout/MainLayout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import injectSheet from 'react-jss';
+
+import styles from './MainLayout.styles';
+
+class MainLayout extends Component {
+  render() {
+    return <div>MainLayout</div>;
+  }
+}
+export default injectSheet(styles)(MainLayout);
--- myexpenses-client/src/index.js
+++ myexpenses-client/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 
 import 'normalize.css';
 import './styles/index.css';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Router = window.require ? MemoryRouter : BrowserRouter;
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
