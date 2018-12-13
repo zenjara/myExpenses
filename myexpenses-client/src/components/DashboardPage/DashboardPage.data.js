b287df0aa212d575a1fd3e1635133f58d64c3053
Kristijan Korac <korac.kristijan@gmail.com>
2018-12-13 22:19:52 +0100

    Dashboard API implemente

--- myexpenses-client/src/components/DashboardPage/DashboardPage.data.js
+++ myexpenses-client/src/components/DashboardPage/DashboardPage.data.js
@@ -1,22 +1,3 @@
-// import { get } from '../../http';
-//
-// export const getDashboardStats = () => get('/stats');
+import { get } from '../../http';
 
-export const getDashboardStats = () => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(dashboardStats), 1400);
-  });
-};
-
-const dashboardStats = [
-  {
-    name: 'Daily stats',
-    limit: 200,
-    spent: 400
-  },
-  {
-    name: 'Montly stats',
-    limit: 3500,
-    spent: 1560
-  }
-];
+export const getDashboardMatrics = () => get('/metrics');
