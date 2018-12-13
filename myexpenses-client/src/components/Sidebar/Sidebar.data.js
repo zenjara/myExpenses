6c285c5df07cd2b20c781830d4f706779498b8ee
Kristijan Korac <korac.kristijan@gmail.com>
2018-12-13 22:20:33 +0100

    Sidebar API implemented

--- myexpenses-client/src/components/Sidebar/Sidebar.data.js
+++ myexpenses-client/src/components/Sidebar/Sidebar.data.js
@@ -1,17 +1,3 @@
-// import { get } from '../../http';
-//
-// export const getUserProfile = () => get('/me');
+import { get } from '../../http';
 
-export const getUserProfile = () => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(userInfo), 1400);
-  });
-};
-
-const userInfo = {
-  id: 2,
-  name: 'Kristijan',
-  email: 'korac.kristijan@gmail.com',
-  daily_limit: null,
-  monthly_limit: null
-};
+export const getUserProfile = () => get('/me');
