5bf819c9efc6ab3164caeb2b53b4ffe3c5c61388
ivanmatas <ivan.matas@arsfutura.co>
2018-12-16 18:44:11 +0100

    Add delete and put to CORS config

--- myexpenses-api-rails/config/application.rb
+++ myexpenses-api-rails/config/application.rb
@@ -32,7 +32,7 @@ module MyexpensesApiRails
 		config.middleware.insert_before 0, Rack::Cors do
 			allow do
 				origins '*'
-				resource '*', :headers => :any, :methods => [:get, :post, :options]
+				resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
 			end
 		end
 		#autoloads lib folder during production
