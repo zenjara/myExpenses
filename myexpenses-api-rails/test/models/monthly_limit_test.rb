6f9f0e1de85d3f0390bfc9089128e607c17ceb62
ivanmatas <ivan.matas@arsfutura.co>
2018-12-18 11:23:53 +0100

    Implement monthly and daily limit model tests

--- myexpenses-api-rails/test/models/monthly_limit_test.rb
+++ myexpenses-api-rails/test/models/monthly_limit_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class MonthlyLimitTest < ActiveSupport::TestCase
-	# test "the truth" do
-	#   assert true
-	# end
+  test "create new monthly limit" do
+    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    monthly_limit = MonthlyLimit.new(amount: 200, currency: 'HRK', user: user)
+    assert monthly_limit.save
+  end
+
+  test "create new monthly limit without amount" do
+    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    monthly_limit = MonthlyLimit.new(currency: 'HRK', user: user)
+    assert_not monthly_limit.save
+  end
+
+  test "create new monthly limit without currency" do
+    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    monthly_limit = MonthlyLimit.new(amount: 200, user: user)
+    assert_not monthly_limit.save
+  end
+
+  test "create new monthly limit without user" do
+    monthly_limit = MonthlyLimit.new(amount: 200, currency: 'HRK')
+    assert_not monthly_limit.save
+  end
 end
