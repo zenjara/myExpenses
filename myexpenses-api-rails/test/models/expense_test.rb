7de12e23badde5cdaa0cd483285a9dd2d70d2808
ivanmatas <ivan.matas@arsfutura.co>
2018-12-18 10:53:47 +0100

    Implement expense model tests

--- myexpenses-api-rails/test/models/expense_test.rb
+++ myexpenses-api-rails/test/models/expense_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
-	# test "the truth" do
-	#   assert true
-	# end
+  test "create new expense" do
+    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    expense_category = ExpenseCategory.create!(name: 'food', user: user)
+    expense = Expense.new(amount: 100, date: Date.today, expense_category: expense_category, user: user)
+    assert expense.save
+  end
+
+  test "create new expense without amount" do
+    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    expense_category = ExpenseCategory.create!(name: 'food', user: user)
+    expense = Expense.new(date: Date.today, expense_category: expense_category, user: user)
+    assert_not expense.save
+  end
+
+  test "create new expense without date" do
+    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    expense_category = ExpenseCategory.create!(name: 'food', user: user)
+    expense = Expense.new(amount: 100, expense_category: expense_category, user: user)
+    assert_not expense.save
+  end
+
+  test "create new expense without expense category" do
+    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    expense = Expense.new(amount: 100, date: Date.today, user: user)
+    assert_not expense.save
+  end
+
+  test "create new expense without user" do
+    expense_category = ExpenseCategory.create(name: 'food')
+    expense = Expense.new(amount: 100, date: Date.today, expense_category: expense_category)
+    assert_not expense.save
+  end
 end
