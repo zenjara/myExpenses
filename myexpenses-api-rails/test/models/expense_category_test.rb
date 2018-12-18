7106278c4d285e9f690bcb35822dad111998f761
ivanmatas <ivan.matas@arsfutura.co>
2018-12-18 11:04:26 +0100

    Implement expense_category model tests

--- myexpenses-api-rails/test/models/expense_category_test.rb
+++ myexpenses-api-rails/test/models/expense_category_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ExpenseCategoryTest < ActiveSupport::TestCase
-	# test "the truth" do
-	#   assert true
-	# end
+  test "create new expense category" do
+    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    expense_category = ExpenseCategory.new(name: 'food', user: user)
+    assert expense_category.save
+  end
+
+  test "create new expense category without name" do
+    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    expense_category = ExpenseCategory.new(user: user)
+    assert_not expense_category.save
+  end
+
+  test "create new expense category without user" do
+    expense_category = ExpenseCategory.new(name: 'food')
+    assert_not expense_category.save
+  end
 end
