7bd044595b0b45ab78f28d9b965a9c8fdb6b96cf
ivanmatas <ivan.matas@arsfutura.co>
2018-12-18 10:56:09 +0100

    Refactor expense model tests

--- myexpenses-api-rails/test/models/expense_test.rb
+++ myexpenses-api-rails/test/models/expense_test.rb
@@ -2,28 +2,28 @@ require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
   test "create new expense" do
-    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
-    expense_category = ExpenseCategory.create!(name: 'food', user: user)
+    user = create_user
+    expense_category = create_expense_category(user)
     expense = Expense.new(amount: 100, date: Date.today, expense_category: expense_category, user: user)
     assert expense.save
   end
 
   test "create new expense without amount" do
-    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
-    expense_category = ExpenseCategory.create!(name: 'food', user: user)
+    user = create_user
+    expense_category = create_expense_category(user)
     expense = Expense.new(date: Date.today, expense_category: expense_category, user: user)
     assert_not expense.save
   end
 
   test "create new expense without date" do
-    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
-    expense_category = ExpenseCategory.create!(name: 'food', user: user)
+    user = create_user
+    expense_category =create_expense_category(user)
     expense = Expense.new(amount: 100, expense_category: expense_category, user: user)
     assert_not expense.save
   end
 
   test "create new expense without expense category" do
-    user = User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+    user = create_user
     expense = Expense.new(amount: 100, date: Date.today, user: user)
     assert_not expense.save
   end
@@ -32,5 +32,15 @@ class ExpenseTest < ActiveSupport::TestCase
     expense_category = ExpenseCategory.create(name: 'food')
     expense = Expense.new(amount: 100, date: Date.today, expense_category: expense_category)
     assert_not expense.save
+  end
+
+  private
+
+  def create_user
+    User.create!(name: 'test user', email: 'test@test.com', password: 'test')
+  end
+
+  def create_expense_category(user)
+    ExpenseCategory.create!(name: 'food', user: user)
   end
 end
