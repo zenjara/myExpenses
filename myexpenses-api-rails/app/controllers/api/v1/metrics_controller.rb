5c740c07cc0a517b10c7eee3fff73883037d6d8d
ivanmatas <ivan.matas@arsfutura.co>
2019-01-07 14:13:25 +0100

    Implement monthly and daily limit model tests

--- myexpenses-api-rails/app/controllers/api/v1/metrics_controller.rb
+++ myexpenses-api-rails/app/controllers/api/v1/metrics_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::MetricsController < ApplicationController
     beginning_of_next_month = beginning_of_month.next_month
 
     metrics = Hash.new
-    metrics['dailyLimit'] = @current_user.daily_limit.amount
-    metrics['monthlyLimit'] = @current_user.monthly_limit.amount
+    metrics['dailyLimit'] = @current_user.daily_limit.amount unless @current_user.daily_limit.nil?
+    metrics['monthlyLimit'] = @current_user.monthly_limit.amount  unless @current_user.monthly_limit.nil?
     metrics['dailyExpenses'] = Expense.where(:user_id => @current_user.id, :date => Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).pluck('amount').compact.sum
     metrics['monthlyExpenses'] = Expense.where(:user_id => @current_user.id, :date => beginning_of_month..beginning_of_next_month).pluck('amount').compact.sum
 
