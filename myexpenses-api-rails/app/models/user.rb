6750c516f7532fef756e4b9027d5de4bdd42973a
ivanmatas <ivan.matas@arsfutura.co>
2019-01-08 14:43:48 +0100

    Implement monthly and daily limit defaults to user creation

--- myexpenses-api-rails/app/models/user.rb
+++ myexpenses-api-rails/app/models/user.rb
@@ -7,6 +7,15 @@ class User < ApplicationRecord
   validates_presence_of :name, :email, :password_digest
   validates :email, uniqueness: true
 
+  after_create :create_limits
+
   #encrypt password
   has_secure_password
+
+  def create_limits
+    daily_limit = self.build_daily_limit(amount: 0, currency: 'HRK')
+    monthly_limit = self.build_monthly_limit(amount: 0, currency: 'HRK')
+    daily_limit.save
+    monthly_limit.save
+  end
 end
